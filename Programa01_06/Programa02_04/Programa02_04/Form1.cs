cabad39ad6bb86e69e3354e25582033ee629bf9a
jose-hr14 <71669454+jose-hr14@users.noreply.github.com>
2021-10-18 19:47:12 +0200

    as

---
+++ Programa01_06/Programa02_04/Programa02_04/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Programa02_04
+{
+    public partial class Form1 : Form
+    {
+        Form2 nuevoFormulario;
+        public Form1()
+        {
+            InitializeComponent();
+            nuevoFormulario = new Form2();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (Application.OpenForms.Count == 1)
+            {
+                nuevoFormulario = new Form2();
+            }
+            nuevoFormulario.Mensaje = textBox1.Text;
+            nuevoFormulario.Show();
+        }
+    }
+}
---
+++ Programa01_06/Programa02_04/Programa02_04/Form2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Programa02_04
+{
+    public partial class Form2 : Form
+    {
+        private string mensaje;
+        private Form1 formulario1;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        public Form2(Form1 formulario)
+        {
+            InitializeComponent();
+            formulario = formulario1;
+        }
+        public Form2(string cadena)
+        {
+            InitializeComponent();
+            lblParametro.Text = cadena;
+        }
+        public string Mensaje
+        {
+            get
+            {
+                return mensaje;
+            }
+            set
+            {
+                mensaje = value;
+                lblParametro.Text = mensaje;
+            }
+        }
+       
+    }
+}
