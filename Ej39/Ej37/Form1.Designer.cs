84c5ea6f569a1276b80d51f43b544bbfe2abe032
jose-hr14 <71669454+jose-hr14@users.noreply.github.com>
2021-11-03 16:50:08 +0100

    Update Form1.Designer.cs

--- Ej39/Ej37/Form1.Designer.cs
+++ Ej39/Ej37/Form1.Designer.cs
@@ -87,12 +87,12 @@
             // 
             this.trackBar1.Location = new System.Drawing.Point(70, 211);
             this.trackBar1.Maximum = 1000;
-            this.trackBar1.Minimum = 1;
+            this.trackBar1.Minimum = 100;
             this.trackBar1.Name = "trackBar1";
             this.trackBar1.Size = new System.Drawing.Size(217, 45);
             this.trackBar1.TabIndex = 4;
             this.trackBar1.TickFrequency = 100;
-            this.trackBar1.Value = 1;
+            this.trackBar1.Value = 100;
             this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
             // 
             // label2
