b4ad94495c71ae222eb7ef13fd5eb34c86023c6a
jose-hr14 <71669454+jose-hr14@users.noreply.github.com>
2021-11-26 17:45:29 +0100

    a

---
+++ Ej54/Ej54/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ej54
+{
+    public partial class Form1 : Form
+    {
+        List<string> lista = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+            lista.Add("uno");
+            lista.Add("dos");
+            lista.Add("tres");
+            listBox1.DataSource = null;
+            listBox1.DataSource = lista;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lista.Add(textBox1.Text);
+            listBox1.DataSource = null;
+            listBox1.DataSource = lista;
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if(lista.Count > 0)
+            {
+                lista.Remove(textBox2.Text);
+                listBox1.DataSource = null;
+                listBox1.DataSource = lista;
+            }            
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string palabra = textBox2.Text;
+            for(int i = 0; i < lista.Count; i++)
+            {
+                if (textBox2.Text == lista[i])
+                {
+                    lista[i] = textBox1.Text;
+                    //lista.RemoveAt(i);
+                    //lista.Insert(i, textBox1.Text);
+                }
+                    
+            }
+            listBox1.DataSource = null;
+            listBox1.DataSource = lista;
+        }
+    }
+}
---
+++ SegundaEvaluacion/SegundaEvaluacion/Program.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SegundaEvaluacion
+{
+    class Program
+    {
+        public static void ListaSinDelegadosNiLambda()
+        {
+            List<int> lista = new List<int>() {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            Console.WriteLine("Todos los números de la lista");
+            foreach(int numero in lista)
+            {
+                Console.Write(numero + " ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Todos los números pares de la lista");
+            foreach (int numero in lista)
+            {
+                if (numero % 2 == 0)
+                    Console.Write(numero + " ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Todos los números de la lista entre 3 y 7");
+            foreach (int numero in lista)
+            {
+                if (numero > 2 && numero < 8)
+                    Console.Write(numero + " ");
+            }
+            Console.WriteLine();
+        }
+        //public static void Ej55()
+        //{
+        //    List<int> lista = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        //    Console.WriteLine("Todos los números pares de la lista");
+        //    foreach (int numero in lista)
+        //    {
+        //        if(numero % 2 == 0)
+        //            Console.Write(numero + " ");
+        //    }
+        //    Console.WriteLine();
+        //}
+        //public static void Ej56()
+        //{
+        //    List<int> lista = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        //    Console.WriteLine("Todos los números de la lista entre 3 y 7");
+        //    foreach (int numero in lista)
+        //    {
+        //        if (numero > 2 && numero < 8)
+        //            Console.Write(numero + " ");
+        //    }
+        //    Console.WriteLine();
+        //}
+        static bool EsPar(int n)
+        {
+            bool esPar;
+            if (n % 2 == 0)
+                esPar = true;
+            else
+                esPar = false;
+            return esPar;
+        }
+        static bool EsRango(int n)
+        {
+            bool esRango;
+            if (n > 2 && n < 8)
+                esRango = true;
+            else
+                esRango = false;
+            return esRango;
+        }
+        static void DelegadosGenericos() //examen
+        {
+            List<int> lista = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            Console.WriteLine("Todos los números de la lista");
+            foreach (int numero in lista)
+            {
+                Console.Write(numero + " ");
+            }
+            Console.WriteLine();
+
+            Predicate<int> delegadoPar = new Predicate<int>(EsPar);
+            List<int> listaPares = lista.FindAll(delegadoPar);
+            Console.WriteLine("Todos los números pares de la lista");
+            foreach (int numero in listaPares)
+            {
+                Console.Write(numero + " ");
+            }
+            Console.WriteLine();
+
+            Predicate<int> delegadoRango = new Predicate<int>(EsRango);
+            List<int> listaRango = lista.FindAll(delegadoRango);
+            Console.WriteLine("Todos los números de la lista entre 3 y 7");
+            foreach (int numero in listaRango)
+            {
+                Console.Write(numero + " ");
+            }
+            Console.WriteLine();
+        }
+        static void Lambda1() //examen
+        {
+            List<int> lista = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            Console.WriteLine("Todos los números de la lista");
+            foreach (int numero in lista)
+            {
+                Console.Write(numero + " ");
+            }
+            Console.WriteLine();
+
+            List<int> listaPares = lista.FindAll(numero => EsPar(numero));
+            Console.WriteLine("Todos los números pares de la lista");
+            foreach (int numero in listaPares)
+            {
+                Console.Write(numero + " ");
+            }
+            Console.WriteLine();
+
+            List<int> listaRango = lista.FindAll(numero => EsRango(numero));
+            Console.WriteLine("Todos los números de la lista entre 3 y 7");
+            foreach (int numero in listaRango)
+            {
+                Console.Write(numero + " ");
+            }
+            Console.WriteLine();
+        }
+        static void lambda2()
+        {
+            List<int> lista = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };            
+            Console.WriteLine("Todos los números de la lista");
+            foreach (int numero in lista)
+            {
+                Console.Write(numero + " ");
+            }
+            Console.WriteLine();
+            
+            List<int> listaPares = lista.FindAll(numero =>
+            {
+                bool esPar;
+                if (numero % 2 == 0)
+                    esPar = true;
+                else
+                    esPar = false;
+                return esPar;
+            });
+            Console.WriteLine("Todos los números pares de la lista");
+            foreach (int numero in listaPares)
+            {
+                Console.Write(numero + " ");
+            }
+            Console.WriteLine();
+
+            List<int> listaRango = lista.FindAll(numero =>
+            {
+                bool esRango;
+                if (numero > 2 && numero < 8)
+                    esRango = true;
+                else
+                    esRango = false;
+                return esRango;
+            });
+            Console.WriteLine("Todos los números de la lista entre 3 y 7");
+            foreach (int numero in listaRango)
+            {
+                Console.Write(numero + " ");
+            }
+            Console.WriteLine();
+        }
+        static void Lambda3()
+        {
+            List<int> lista = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            Console.WriteLine("Todos los números de la lista");
+            foreach (int numero in lista)
+            {
+                Console.Write(numero + " ");
+            }
+            Console.WriteLine();
+
+            List<int> listaPares = lista.FindAll(numero => (numero % 2) == 0);
+            Console.WriteLine("Todos los números pares de la lista");
+            foreach (int numero in listaPares)
+            {
+                Console.Write(numero + " ");
+            }
+            Console.WriteLine();
+
+            List<int> listaRango = lista.FindAll(numero => (numero > 2 && numero < 8));
+            Console.WriteLine("Todos los números de la lista entre 3 y 7");
+            foreach (int numero in listaRango)
+            {
+                Console.Write(numero + " ");
+            }
+            Console.WriteLine();
+        }
+        static void Main()
+        {
+            Lambda3();
+        }
+    }
+}
