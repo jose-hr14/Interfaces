7c4ff8d5fb9118009a3bd9993619009e9c0f8f01
jose-hr14 <71669454+jose-hr14@users.noreply.github.com>
2021-12-13 16:18:06 +0100

    a

---
+++ Ej56/Ej55/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Ej55
+{
+    public partial class Form1 : Form
+    {
+        List<ClaseNombre> lista;
+        public Form1()
+        {
+            InitializeComponent();
+
+            lista = new List<ClaseNombre>();
+            lista.Add(new ClaseNombre("uno"));
+            lista.Add(new ClaseNombre("dos"));
+            lista.Add(new ClaseNombre("tres"));           
+            dgvNumeros.DataSource = null;
+            dgvNumeros.DataSource = lista;
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            /*
+             * chars int32
+             * length
+             * lista con grid view solo conecta la longitud de los strings
+             * podemos encapsular el string en una clase usando lambda
+             */
+            if(txbNuevoNombre.Text.Length > 0)
+            {
+                lista.Add(new ClaseNombre(txbNuevoNombre.Text));
+                dgvNumeros.DataSource = null;
+                dgvNumeros.DataSource = lista;
+            }            
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            //int indice = lista.FindIndex(nombre => nombre.Nombre == txbNombreProcesar.Text);
+            int indice = lista.FindIndex(nombre => nombre.Nombre.Contains(txbNombreProcesar.Text));
+            if (indice > -1)
+            {
+                lista[indice] = new ClaseNombre(txbNuevoNombre.Text);                
+                dgvNumeros.DataSource = null;
+                dgvNumeros.DataSource = lista;
+            }
+            
+        }
+
+        private void btnQuitar_Click(object sender, EventArgs e)
+        {
+            int indice = lista.FindIndex(nombre => nombre.Nombre == txbNombreProcesar.Text);
+            if (indice > -1)
+            {
+                lista.RemoveAt(indice);                
+                dgvNumeros.DataSource = null;
+                dgvNumeros.DataSource = lista;
+            }
+        }
+        public class ClaseNombre
+        {
+            string nombre;
+
+            public ClaseNombre(string nombre)
+            {
+                this.nombre = nombre;
+            }
+
+            public string Nombre
+            {
+                get
+                {
+                    return nombre;
+                }
+                set
+                {
+                    nombre = value;
+                }
+            }
+        }
+    }
+}
---
+++ Ej60-TextoColorinesConBoton/Ej60-TextoColorinesConBoton/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ej60_TextoColorinesConBoton
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnColor_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            colorDialog.ShowDialog();
+            Color color =  colorDialog.Color;
+            lblTextoPrueba.ForeColor = color;
+        }
+    }
+}
