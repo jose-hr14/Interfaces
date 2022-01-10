c7700906994e3fb497d9fb4892861eaf4cc481d1
jose-hr14 <71669454+jose-hr14@users.noreply.github.com>
2022-01-10 17:07:59 +0100

    a

--- Programa07_02/Programa07_02/Form1.Designer.cs
+++ Programa07_02/Programa07_02/Form1.Designer.cs
@@ -30,26 +30,29 @@ namespace Programa07_02
         private void InitializeComponent()
         {
             this.grbMaskedTextBox = new System.Windows.Forms.GroupBox();
-            this.grbTextBox = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.txbNombre1 = new System.Windows.Forms.TextBox();
-            this.txbEdad1 = new System.Windows.Forms.TextBox();
             this.btnProcesar1 = new System.Windows.Forms.Button();
-            this.btnProcesar2 = new System.Windows.Forms.Button();
-            this.mktxbNombre = new System.Windows.Forms.MaskedTextBox();
+            this.txbEdad1 = new System.Windows.Forms.TextBox();
+            this.txbNombre1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.grbTextBox = new System.Windows.Forms.GroupBox();
             this.mktxbEdad = new System.Windows.Forms.MaskedTextBox();
+            this.mktxbNombre = new System.Windows.Forms.MaskedTextBox();
+            this.btnProcesar2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblProcesado1 = new System.Windows.Forms.Label();
+            this.lblProcesado2 = new System.Windows.Forms.Label();
             this.grbMaskedTextBox.SuspendLayout();
             this.grbTextBox.SuspendLayout();
             this.SuspendLayout();
             // 
             // grbMaskedTextBox
             // 
+            this.grbMaskedTextBox.Controls.Add(this.lblProcesado1);
+            this.grbMaskedTextBox.Controls.Add(this.mktxbEdad);
             this.grbMaskedTextBox.Controls.Add(this.btnProcesar1);
-            this.grbMaskedTextBox.Controls.Add(this.txbEdad1);
-            this.grbMaskedTextBox.Controls.Add(this.txbNombre1);
+            this.grbMaskedTextBox.Controls.Add(this.mktxbNombre);
             this.grbMaskedTextBox.Controls.Add(this.label2);
             this.grbMaskedTextBox.Controls.Add(this.label1);
             this.grbMaskedTextBox.Location = new System.Drawing.Point(13, 13);
@@ -59,29 +62,31 @@ namespace Programa07_02
             this.grbMaskedTextBox.TabStop = false;
             this.grbMaskedTextBox.Text = "Masked TextBox";
             // 
-            // grbTextBox
+            // btnProcesar1
             // 
-            this.grbTextBox.Controls.Add(this.mktxbEdad);
-            this.grbTextBox.Controls.Add(this.mktxbNombre);
-            this.grbTextBox.Controls.Add(this.btnProcesar2);
-            this.grbTextBox.Controls.Add(this.label4);
-            this.grbTextBox.Controls.Add(this.label3);
-            this.grbTextBox.Location = new System.Drawing.Point(13, 192);
-            this.grbTextBox.Name = "grbTextBox";
-            this.grbTextBox.Size = new System.Drawing.Size(700, 161);
-            this.grbTextBox.TabIndex = 1;
-            this.grbTextBox.TabStop = false;
-            this.grbTextBox.Text = "TextBox";
+            this.btnProcesar1.Location = new System.Drawing.Point(455, 67);
+            this.btnProcesar1.Name = "btnProcesar1";
+            this.btnProcesar1.Size = new System.Drawing.Size(75, 23);
+            this.btnProcesar1.TabIndex = 4;
+            this.btnProcesar1.Text = "Procesar";
+            this.btnProcesar1.UseVisualStyleBackColor = true;
+            this.btnProcesar1.Click += new System.EventHandler(this.btnProcesar1_Click);
             // 
-            // label1
+            // txbEdad1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(70, 47);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(176, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Nombre (máximo cinco carácteres): ";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.txbEdad1.Location = new System.Drawing.Point(275, 79);
+            this.txbEdad1.Name = "txbEdad1";
+            this.txbEdad1.Size = new System.Drawing.Size(100, 20);
+            this.txbEdad1.TabIndex = 3;
+            this.txbEdad1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txbEdad1_KeyPress);
+            // 
+            // txbNombre1
+            // 
+            this.txbNombre1.Location = new System.Drawing.Point(275, 41);
+            this.txbNombre1.Name = "txbNombre1";
+            this.txbNombre1.Size = new System.Drawing.Size(100, 20);
+            this.txbNombre1.TabIndex = 2;
+            this.txbNombre1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txbNombre1_KeyPress);
             // 
             // label2
             // 
@@ -93,15 +98,55 @@ namespace Programa07_02
             this.label2.Text = "Edad (máximo dos dígitos): ";
             this.label2.TextAlign = System.Drawing.ContentAlignment.TopRight;
             // 
-            // label3
+            // label1
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(109, 86);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(137, 13);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Edad (máximo dos dígitos): ";
-            this.label3.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(70, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(176, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre (máximo cinco carácteres): ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // grbTextBox
+            // 
+            this.grbTextBox.Controls.Add(this.lblProcesado2);
+            this.grbTextBox.Controls.Add(this.btnProcesar2);
+            this.grbTextBox.Controls.Add(this.label4);
+            this.grbTextBox.Controls.Add(this.label3);
+            this.grbTextBox.Controls.Add(this.txbEdad1);
+            this.grbTextBox.Controls.Add(this.txbNombre1);
+            this.grbTextBox.Location = new System.Drawing.Point(13, 192);
+            this.grbTextBox.Name = "grbTextBox";
+            this.grbTextBox.Size = new System.Drawing.Size(700, 161);
+            this.grbTextBox.TabIndex = 1;
+            this.grbTextBox.TabStop = false;
+            this.grbTextBox.Text = "TextBox";
+            // 
+            // mktxbEdad
+            // 
+            this.mktxbEdad.Location = new System.Drawing.Point(275, 80);
+            this.mktxbEdad.Name = "mktxbEdad";
+            this.mktxbEdad.Size = new System.Drawing.Size(100, 20);
+            this.mktxbEdad.TabIndex = 8;
+            // 
+            // mktxbNombre
+            // 
+            this.mktxbNombre.Location = new System.Drawing.Point(275, 44);
+            this.mktxbNombre.Name = "mktxbNombre";
+            this.mktxbNombre.Size = new System.Drawing.Size(100, 20);
+            this.mktxbNombre.TabIndex = 7;
+            this.mktxbNombre.ValidatingType = typeof(int);
+            // 
+            // btnProcesar2
+            // 
+            this.btnProcesar2.Location = new System.Drawing.Point(455, 63);
+            this.btnProcesar2.Name = "btnProcesar2";
+            this.btnProcesar2.Size = new System.Drawing.Size(75, 23);
+            this.btnProcesar2.TabIndex = 6;
+            this.btnProcesar2.Text = "Procesar";
+            this.btnProcesar2.UseVisualStyleBackColor = true;
+            this.btnProcesar2.Click += new System.EventHandler(this.btnProcesar2_Click);
             // 
             // label4
             // 
@@ -113,51 +158,33 @@ namespace Programa07_02
             this.label4.Text = "Nombre (máximo cinco carácteres): ";
             this.label4.TextAlign = System.Drawing.ContentAlignment.TopRight;
             // 
-            // txbNombre1
+            // label3
             // 
-            this.txbNombre1.Location = new System.Drawing.Point(298, 47);
-            this.txbNombre1.Name = "txbNombre1";
-            this.txbNombre1.Size = new System.Drawing.Size(100, 20);
-            this.txbNombre1.TabIndex = 2;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(109, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(137, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Edad (máximo dos dígitos): ";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopRight;
             // 
-            // txbEdad1
+            // lblProcesado1
             // 
-            this.txbEdad1.Location = new System.Drawing.Point(298, 83);
-            this.txbEdad1.Name = "txbEdad1";
-            this.txbEdad1.Size = new System.Drawing.Size(100, 20);
-            this.txbEdad1.TabIndex = 3;
+            this.lblProcesado1.AutoSize = true;
+            this.lblProcesado1.Location = new System.Drawing.Point(275, 112);
+            this.lblProcesado1.Name = "lblProcesado1";
+            this.lblProcesado1.Size = new System.Drawing.Size(74, 13);
+            this.lblProcesado1.TabIndex = 9;
+            this.lblProcesado1.Text = "lblProcesado1";
             // 
-            // btnProcesar1
+            // lblProcesado2
             // 
-            this.btnProcesar1.Location = new System.Drawing.Point(455, 67);
-            this.btnProcesar1.Name = "btnProcesar1";
-            this.btnProcesar1.Size = new System.Drawing.Size(75, 23);
-            this.btnProcesar1.TabIndex = 4;
-            this.btnProcesar1.Text = "Procesar";
-            this.btnProcesar1.UseVisualStyleBackColor = true;
-            // 
-            // btnProcesar2
-            // 
-            this.btnProcesar2.Location = new System.Drawing.Point(455, 63);
-            this.btnProcesar2.Name = "btnProcesar2";
-            this.btnProcesar2.Size = new System.Drawing.Size(75, 23);
-            this.btnProcesar2.TabIndex = 6;
-            this.btnProcesar2.Text = "Procesar";
-            this.btnProcesar2.UseVisualStyleBackColor = true;
-            // 
-            // mktxbNombre
-            // 
-            this.mktxbNombre.Location = new System.Drawing.Point(298, 41);
-            this.mktxbNombre.Name = "mktxbNombre";
-            this.mktxbNombre.Size = new System.Drawing.Size(100, 20);
-            this.mktxbNombre.TabIndex = 7;
-            // 
-            // mktxbEdad
-            // 
-            this.mktxbEdad.Location = new System.Drawing.Point(298, 86);
-            this.mktxbEdad.Name = "mktxbEdad";
-            this.mktxbEdad.Size = new System.Drawing.Size(100, 20);
-            this.mktxbEdad.TabIndex = 8;
+            this.lblProcesado2.AutoSize = true;
+            this.lblProcesado2.Location = new System.Drawing.Point(275, 106);
+            this.lblProcesado2.Name = "lblProcesado2";
+            this.lblProcesado2.Size = new System.Drawing.Size(74, 13);
+            this.lblProcesado2.TabIndex = 7;
+            this.lblProcesado2.Text = "lblProcesado2";
             // 
             // Form1
             // 
@@ -190,6 +217,8 @@ namespace Programa07_02
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.MaskedTextBox mktxbEdad;
         private System.Windows.Forms.MaskedTextBox mktxbNombre;
+        private System.Windows.Forms.Label lblProcesado1;
+        private System.Windows.Forms.Label lblProcesado2;
     }
 }
 
--- Programa07_02/Programa07_02/Form1.cs
+++ Programa07_02/Programa07_02/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +16,72 @@ namespace Programa07_02
         public Form1()
         {
             InitializeComponent();
+
+            //mktxbNombre.Mask = "LLLLL"; //opcional
+            //mktxbEdad.Mask = "00"; //opcional
+
+            mktxbNombre.Mask = "99999"; //opcional
+            mktxbEdad.Mask = "99"; //opcional
+
+            mktxbNombre.PromptChar = ' '; //ocultar la vista previal del masked textbox
+            mktxbEdad.PromptChar = ' '; //ocultar la vista previal del masked textbox
+        }
+
+        private void btnProcesar1_Click(object sender, EventArgs e)
+        {
+            if(mktxbNombre.Text.Length > 0 && mktxbEdad.Text.Length > 0)
+            {
+                lblProcesado1.Text = mktxbNombre.Text + " tiene una edad de " + mktxbEdad.Text;
+            }
+        }
+
+        private void btnProcesar2_Click(object sender, EventArgs e)
+        {
+            if (mktxbNombre.Text.Length > 0 && mktxbEdad.Text.Length > 0)
+            {
+                lblProcesado1.Text = mktxbNombre.Text + " tiene una edad de " + mktxbEdad.Text;
+            }
+        }
+
+        private void txbNombre1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            txbNombre1.MaxLength = 5; //Para limitar los carácteres a 5
+
+            bool esNombreCorrecto;
+
+            esNombreCorrecto = Regex.IsMatch(e.KeyChar.ToString(), "^[a-z|A-Z]$");
+
+            //tecla 8 es tecla de borrar, y el 32 es el espacio en blanco
+            //se permitirá escribir 
+            if(esNombreCorrecto || e.KeyChar == (char)8 || e.KeyChar == (char)32)
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txbEdad1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            txbEdad1.MaxLength = 2; //Para limitar los carácteres a 5
+
+            bool noEsEdadCorrecta;
+
+            //esNombreCorrecto = Regex.IsMatch(e.KeyChar.ToString(), "^[^0-9]$"); cuando ponemos el ^, significa el contrario, como el ! de los bool
+            noEsEdadCorrecta = Regex.IsMatch(e.KeyChar.ToString(), "^[^0-9]$");
+
+            //tecla 8 es tecla de borrar, y el 32 es el espacio en blanco
+            //se permitirá escribir 
+            if (noEsEdadCorrecta && e.KeyChar != (char)8)
+            {
+                e.Handled = true;
+            }
+            else
+            {
+                e.Handled = false;
+            }
         }
     }
 }
