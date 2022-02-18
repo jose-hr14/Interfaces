4c81767ef021b22c94512f398fa211aa060e08e3
jose-hr14 <71669454+jose-hr14@users.noreply.github.com>
2022-02-18 17:35:39 +0100

    a

--- Febrero04_Access/Febrero04_Access/Libreria.cs
+++ Febrero04_Access/Febrero04_Access/Libreria.cs
@@ -23,9 +23,11 @@ namespace Febrero04_Access
             dataGridView1.AllowUserToAddRows = false;
             dataGridView1.AllowUserToDeleteRows = false;
             dataGridView1.ReadOnly = true;
+            dataGridView1.RowHeadersVisible = false;
             this.form1 = form1;
             conexionConLaBD = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;
             Data Source=C:\Users\José\Documents\GitHub\Interfaces\Febrero04_Access\Access_DbLibros.accdb");
+            conexionConLaBD = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\hernandez21107\Documents\GitHub\Interfaces\Febrero04_Access\Access_DbLibros.accdb");
             RellenarTabla();
         }
 
